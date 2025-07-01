4f0f03f4dee4821996e066fa5c06ca157a161edc
PichetG9 <yosxi123@gmail.com>
2025-07-01 11:57:15 +0700

    Create first.cpp

---
+++ first.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string s = "                ";
+  cout << endl << endl << "\n\n\n\n";
+  cout << s << " **   ** " << endl;
+  cout << s << "*********" << endl;
+  cout << s << " ******* " << endl;
+  cout << s << "  *****  " << endl;
+  cout << s << "   ***   " << endl;
+  cout << s << "    *    " << endl;
+  
+  return 0;
+}
